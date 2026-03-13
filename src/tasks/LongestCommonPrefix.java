1b34a55a854c9bfa351382d6c6ab90ca8a567015
Aleksey Novikov <alex012120@gmail.com>
2026-03-13 16:10:44 +0300

    LongestCommonPrefix HorizontalScanning solution

--- src/tasks/LongestCommonPrefix.java
+++ src/tasks/LongestCommonPrefix.java
@@ -12,13 +12,43 @@ public class LongestCommonPrefix implements Task {
     @Override
     public void solve() {
         String[] input = {"flower","flow","flight"};
-        String result = longestCommonPrefixVerticalScanning(input);
+        String result = longestCommonPrefixUsingIndexOf(input);
         System.out.println(result);
     }
 
-    private static String longestCommonPrefixVerticalScanning(String[] strs) {
-        StringBuilder answer = new StringBuilder();
+    private static String longestCommonPrefixUsingIndexOf(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
 
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    private static String longestCommonPrefixHorizontalScanning(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!prefix.isEmpty()) {
+                if (strs[i].length() < prefix.length() || !prefix.equals(strs[i].substring(0, prefix.length()))) {
+                    prefix = prefix.substring(0, prefix.length() - 1);
+                } else break;
+            }
+        }
+
+        return prefix;
+    }
+
+    private static String longestCommonPrefixVerticalScanning(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < strs[0].length(); i++) {
             char current = strs[0].charAt(i);
             for (int j = 1 ; j < strs.length; j++) {
