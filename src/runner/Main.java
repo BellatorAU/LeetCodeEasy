72da76a398c6be887b5da2d36d69d64657164af9
Aleksey Novikov <alex012120@gmail.com>
2026-03-17 17:31:50 +0300

    RemoveDuplicatesFromSortedArray solution + null check

--- src/runner/Main.java
+++ src/runner/Main.java
@@ -5,7 +5,7 @@ import tasks.*;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new LongestCommonPrefix();
+        Task task = new RemoveDuplicatesFromSortedArray();
         task.solve();
     }
 }
--- src/tasks/RemoveDuplicatesFromSortedArray.java
+++ src/tasks/RemoveDuplicatesFromSortedArray.java
@@ -1,2 +1,28 @@
-package tasks;public class RemoveDuplicatesFromSortedArray {
+package tasks;
+
+import core.Task;
+
+public class RemoveDuplicatesFromSortedArray implements Task {
+    private static final int TASK_ID = 26;
+
+    @Override
+    public void solve() {
+        int[] nums = {1,1,2};
+        int result = removeDuplicates(nums);
+        System.out.println(result);
+    }
+
+    private static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int counter = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[counter] != nums[i]) {
+                counter++;
+                nums[counter] = nums[i];
+            }
+        }
+
+        return counter + 1;
+    }
 }
