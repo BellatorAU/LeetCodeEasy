4fa576a5509e2c0f6052bab5e2bfec4ae9ab9951
Aleksey Novikov <alex012120@gmail.com>
2026-03-17 17:46:03 +0300

    RemoveElement (id=27) solution

--- src/runner/Main.java
+++ src/runner/Main.java
@@ -5,7 +5,7 @@ import tasks.*;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new RemoveDuplicatesFromSortedArray();
+        Task task = new RemoveElement();
         task.solve();
     }
 }
--- src/tasks/RemoveElement.java
+++ src/tasks/RemoveElement.java
@@ -1,2 +1,29 @@
-package tasks;public class RemoveElement {
+package tasks;
+
+import core.Task;
+
+public class RemoveElement implements Task {
+    private static final int TASK_ID = 27;
+
+    @Override
+    public void solve() {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        int result = removeElement(nums, val);
+        System.out.println(result);
+    }
+
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0;
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+
+        return counter;
+    }
 }
